bbb3da37853698dd1902aaec164dc8571fc0db88
Mohamad Shiralizadeh <mohamad.shir@gmail.com>
2023-04-18 09:40:01 +0200

    Fix build issues

--- global-top.vue
+++ global-top.vue
@@ -1,6 +1,6 @@
 <!-- global-bottom.vue -->
 <template>
-    <img src="images/logo.png" class="io-logo absolute top-5 left-5 w-15" v-bind:data-index="$slidev.nav.currentPage" />
+    <img src="/images/logo.png" class="io-logo absolute top-5 left-5 w-15" v-bind:data-index="$slidev.nav.currentPage" />
     <div class="absolute bottom-2 left-10 ">
         <SlideCurrentNo class="text-xs" />
         <span class="text-xs pl-10">Solid.js Journey</span>
